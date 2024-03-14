5b139ace0bb4e1a436b7ce0b3e46bb4765ea766f
oji3 <oji3xx@gmail.com>
2024-03-14 23:53:12 +0800

    fix

--- components/mint.tsx
+++ components/mint.tsx
@@ -164,7 +164,7 @@ export function Mint({ totalSupply, mintedList, disabled }: { totalSupply: strin
             >
               NFT Details
             </p>
-            <button onClick={() => setOpen(false)} className={css({ cursor: "pointer" })}>
+            <button onClick={() => setOpen(false)} className={css({ cursor: "pointer", _hover: { "& svg path": { stroke: "gray.500" } } })}>
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
                 <path d="M18 6L6 18" stroke="#B3B2B1" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                 <path d="M6 6L18 18" stroke="#B3B2B1" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
