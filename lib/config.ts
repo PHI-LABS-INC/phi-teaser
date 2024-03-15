1fdbfb163931a3db4e24ce330e7c35b694a9a55f
oji3 <oji3xx@gmail.com>
2024-03-15 09:31:52 +0800

    fix

--- lib/config.ts
+++ lib/config.ts
@@ -1,3 +1,3 @@
 export const phiTeaserNFTContract = "0x2021Be051aE5E17A9Dc9c3286223b7d6cf2eCcC9" as const;
+export const walletConnectProjectID = "6c37429d912cb97065107c0f849bc879";
 export const alchemyID = "your-alchemy-id";
-export const walletConnectProjectID = process.env.NEXT_PUBLIC_WC_PROJECT_ID!;
