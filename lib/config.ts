5dd4b3cc772f5378089d076e03c8c05fcd784c10
oji3 <oji3xx@gmail.com>
2024-03-15 13:17:07 +0800

    fix

--- lib/config.ts
+++ lib/config.ts
@@ -1,3 +1,4 @@
 export const phiTeaserNFTContract = "0x2021Be051aE5E17A9Dc9c3286223b7d6cf2eCcC9" as const;
 export const walletConnectProjectID = "6c37429d912cb97065107c0f849bc879";
 export const alchemyID = "your-alchemy-id";
+export const frontendURL = "https://phiprotocol.com";
