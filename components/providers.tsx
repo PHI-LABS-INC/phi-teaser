421e5476cde10582cbb535b0f2199a61dea5ad41
oji3 <oji3xx@gmail.com>
2024-03-16 21:40:15 +0800

    fix

--- components/providers.tsx
+++ components/providers.tsx
@@ -16,6 +16,11 @@ createWeb3Modal({
   themeMode: "light",
   enableAnalytics: false,
   enableOnramp: false,
+  featuredWalletIds: [
+    "ecc4036f814562b41a5268adc86270fba1365471402006302e70169465b7ac18",
+    "c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96",
+  ],
+  allWallets: "ONLY_MOBILE",
 });
 
 export function Providers(props: { children: React.ReactNode }) {
