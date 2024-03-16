b627fa7a3dcb3b9a5205f13caf876f1a88d86dd4
oji3 <oji3xx@gmail.com>
2024-03-16 20:01:39 +0800

    fix

--- lib/wagmi.ts
+++ lib/wagmi.ts
@@ -1,14 +1,21 @@
 import { http, createConfig } from "wagmi";
 import { base, sepolia } from "wagmi/chains";
 import { injected, walletConnect } from "wagmi/connectors";
-import { alchemyID, wcProjectID } from "./config";
+import { alchemyID, frontendURL, wcProjectID } from "./config";
+
+const metadata = {
+  name: "Phi",
+  description: "A decentralized identity layer for web3, empowering people to express their on-chain identity.",
+  url: frontendURL,
+  icons: [frontendURL + "/logo.svg"],
+};
 
 export const config = createConfig({
   chains: [
     // base,
     sepolia,
   ],
-  connectors: [injected(), walletConnect({ projectId: wcProjectID, showQrModal: false })],
+  connectors: [injected(), walletConnect({ projectId: wcProjectID, metadata, showQrModal: false })],
   ssr: true,
   transports: {
     // [base.id]: http(),
