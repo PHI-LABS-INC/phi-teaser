121f3f6c5fe51b77049f6f488ed61de1acd62f58
oji3 <oji3xx@gmail.com>
2024-03-25 22:26:00 +0800

    fix

--- lib/config.ts
+++ lib/config.ts
@@ -2,7 +2,7 @@ if (!process.env.NEXT_PUBLIC_ALCHEMY_API_KEY || !process.env.NEXT_PUBLIC_WC_PROJ
   throw new Error("Required env are not set.");
 }
 
-export const phiTeaserNFTContract = "0xf5fb161159d661DedC946d7B88Ad1FF84d837CdA" as const;
+export const phiTeaserNFTContract = "0xc649989246FAa59bBefA7c65551cc4461E823320" as const;
 export const alchemyID = process.env.NEXT_PUBLIC_ALCHEMY_API_KEY;
 export const wcProjectID = process.env.NEXT_PUBLIC_WC_PROJECT_ID!;
 export const frontendURL = process.env.NEXT_PUBLIC_FRONTEND_URL!;
